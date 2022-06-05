ec2aabaff5ac008f936c53ddb7875d7699496cbc
musa <musawenkosit@tfg.co.za>
2022-06-05 21:18:12 +0200

    1

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import models
 from database import engine
 from routers import signin, signup, post, comment, comment_reply, friend_system, user, message
@@ -7,6 +8,15 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(signin.router)
 app.include_router(signup.router)
 app.include_router(post.router)
