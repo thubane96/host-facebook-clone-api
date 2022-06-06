2014c3b1940d15c0e2054ff5bfec2bcf4585e5b8
musa <musawenkosit@tfg.co.za>
2022-06-06 13:50:39 +0200

    added a module

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ from database import Base
 
 class User(Base):
     __tablename__ = "users"
-    id = Column(Integer, primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True, index=True)
     first_name = Column(String, nullable=False)
     last_name = Column(String, nullable=False)
     gender = Column(String, nullable=False)
